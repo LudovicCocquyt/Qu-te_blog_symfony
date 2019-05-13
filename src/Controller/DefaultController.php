36a8aeea0534132913c783e7b5fab2b34789c08b
LudovicCocquyt <cocquyt.ludovic@laposte.net>
2019-05-13 16:12:24 +0200

    mon blog

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{
+	/**
+     * @Route("/", name="app_index")
+    */
+    public function index()
+    {
+   		 return $this->render('blog/default.html.twig', [
+            'titre' => 'Bienvenue sur mon blog',
+    	]);
+    }
+}
