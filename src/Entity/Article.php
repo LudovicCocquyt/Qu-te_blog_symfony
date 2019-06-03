5439adb09c888a71e8750d5a4b0491a09682b1f2
LudovicCocquyt <cocquyt.ludovic@laposte.net>
2019-06-03 14:14:39 +0200

    15 - Symfony : Introduction aux “Services”

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -41,6 +41,11 @@ class Article
      */
     private $tags;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->tags = new ArrayCollection();
@@ -111,6 +116,18 @@ class Article
             $this->tags->removeElement($tag);
             $tag->removeArticle($this);
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
